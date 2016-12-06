7222cd6359f9617562c802ac618e593b68c0b6d7
nwre87 <nwre87@gmail.com>
2016-12-05 16:06:53 -1000

    add babel and style loaders, bundle and webpack config

---
+++ server/bundle.js
@@ -0,0 +1,32 @@
+const Webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const webpackConfig = require('../../webpack.config.js');
+const path = require('path');
+const fs = require('fs');
+const mainPath = path.resolve(__dirname, '..', 'src', 'index.js');
+
+module.exports = () => {
+  let bundleStart = null;
+  const compiler = Webpack(webpackConfig);
+
+  compiler.plugin('compile', () => {
+    console.log('Bundling...');
+    bundleStart = Date.now();
+  });
+
+  compiler.plugin('done', () => {
+    console.log('Bundled in ' + (Date.now() - bundleStart) + 'ms!');
+  });
+
+  const bundler = new WebpackDevServer(compiler, {
+    publicPath: '/build/',
+    hot: true,
+    quiet: false,
+    noInfo: true,
+    stats: {
+      colors: true,
+    }
+  });
+
+  bundler.listen(8080, 'localhost', () => { console.log('Building project please wait...') })
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,36 @@
-const webpack = require('webpack');
+const Webpack = require('webpack');
 const path = require('path');
+const nodeModulesPath = path.resolve(__dirname, 'node_modules');
+const buildPath = path.resolve(__dirname, 'public', 'build');
+const mainPath = path.resolve(__dirname, 'src', 'index.js')
 
-module.exports = {
-  entry: '',
+const config = {
+  devtool: 'eval',
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    mainPath,
+  ],
+  output: {
+    path: buildPath,
+    filename: 'bundle.js',
+    publicPath: '/build/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: [nodeModulesPath],
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css',
+
+      }
+    ],
+  },
+  plugins: [new Webpack.HotModuleReplacementPlugin()]
 };
+
+module.exports = config;
