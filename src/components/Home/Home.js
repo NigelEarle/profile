977428817b073831a8702b23b2a27f28455a72b8
nwre87 <nwre87@gmail.com>
2016-12-12 11:48:30 -1000

    add css-modules

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import styles from './Home.css';
 
 export class HomeComponent extends React.Component{
   render(){
     return (
-      <div>
+      <div className={styles.container}>
         <h1>Home Component!!</h1>
       </div>
     );
