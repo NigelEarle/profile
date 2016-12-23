902c1ee70ce6e3276b253a078b83511c6906d4a5
nwre87 <nwre87@gmail.com>
2016-12-23 13:01:24 -1000

    rough working hamburger menu media query

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,18 @@ import styles from './Header.css';
 const HeaderComponent = () => {
   return(
     <div className={styles.container}>
-      <span><Link to={'/'} className={styles.homeLink}>Hi, I'm Nigel</Link></span>
-      <span><Link to={'/work'} className={styles.link}>Work</Link></span>
-      <span><Link to={'/blog'} className={styles.link}>Blog</Link></span>
-      <span><Link to={'/contact'} className={styles.link}>Contact</Link></span>
+      <Link to={'/'} className={styles.homeLink}>Home Link</Link>
+      <div className={styles.linkContainer}>
+        <Link to={'/work'} className={styles.link}>Work</Link>
+        <Link to={'/blog'} className={styles.link}>Blog</Link>
+        <Link to={'/contact'} className={styles.link}>Contact</Link>
+      </div>
+
+      <div className={styles.hamburger}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
     </div>
   );
 }
