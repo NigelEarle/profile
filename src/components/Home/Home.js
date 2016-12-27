68d56d12ad0b5079b01238bbc529519a1d2e830c
nwre87 <nwre87@gmail.com>
2016-12-27 12:44:56 -1000

    add small about message

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,8 +8,11 @@ const HomeComponent = () => {
       <HeaderComponent />
       <div className={styles.container}>
         <div className={styles.intro}>
-          <h1 className={styles.heading}>Heading</h1>
-          <h2 className={styles.heading}>Sub heading</h2>
+          <h1 className={styles.heading}>Nigel Earle</h1>
+          <h2 className={styles.heading}>Software Developer</h2>
+          <p>
+            I'm a Kailua, Hawaii based JavaScript Software Developer. I make web applications using React, Redux and Node.js. Also, some experience using Ruby and Python.
+          </p>
         </div>
       </div>
     </div>
