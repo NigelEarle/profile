00e989cae28803b3cb3ecfbcb7a489f5de969903
nwre87 <nwre87@gmail.com>
2016-12-26 14:23:55 -1000

    fix header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router';
 import styles from './Header.css';
 
-const HeaderComponent = () => {
-  return(
-    <div className={styles.container}>
-      <Link to={'/'} className={styles.homeLink}>Home Link</Link>
-      <div className={styles.linkContainer}>
-        <Link to={'/work'} className={styles.link}>Work</Link>
-        <Link to={'/blog'} className={styles.link}>Blog</Link>
-        <Link to={'/contact'} className={styles.link}>Contact</Link>
-      </div>
+export class HeaderComponent extends Component{
+  handleMenu() {
+  }
 
-      <div className={styles.hamburger}>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
-        <div className={styles.bar}></div>
+  render () {
+    return(
+      <div className={styles.container}>
+        <Link to={'/'} className={styles.homeLink}>Hi, I'm Nigel</Link>
+        <div className={styles.linkContainer}>
+          <Link to={'/work'} className={styles.link}>Portfolio</Link>
+          <Link to={'/blog'} className={styles.link}>Blog</Link>
+          <Link to={'/contact'} className={styles.link}>Contact</Link>
+        </div>
+
+        <div className={styles.hamburger} onClick={this.handleMenu.bind(this)}>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+          <div className={styles.bar}></div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default HeaderComponent;
