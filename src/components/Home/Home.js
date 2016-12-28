59280dae3843555cef92a92851a6b94b8b50fd26
nwre87 <nwre87@gmail.com>
2016-12-27 17:59:48 -1000

    add description

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,7 @@ const HomeComponent = () => {
           <img src={DEFAULT_PERSON} alt="default person" className={styles.profileImage}/>
           <h1 className={styles.heading}>Nigel Earle</h1>
           <h2 className={styles.heading}>Software Developer</h2>
-          <p>
+          <p className={styles.description}>
             I am a JavaScript Software Developer based in Kailua, Hawaii. I make web applications using React, Redux and Node.js. Also, some experience with Ruby and Python.
           </p>
         </div>
