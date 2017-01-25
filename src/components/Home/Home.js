b7146f4eb292b168d11d53251f093f5a16c8bfef
nwre87 <nwre87@gmail.com>
2017-01-24 14:13:24 -1000

    add basic content for home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,13 @@ const HomeComponent = () => {
     <div>
       <HeaderComponent />
       <div className={styles.container}>
-        Hello
+        <div className={styles.intro}>
+          <h1 className={styles.heading}>Nigel Earle</h1>
+          <h2 className={styles.subHeading}>Sofware Developer</h2>
+          <p className={styles.description}>
+            Software Developer specializing in JavaScript & Node.js. As well as, other server side languages such as Python and Ruby. With some experience and strong intrest in DevOps.
+          </p>
+        </div>
       </div>
     </div>
   );
