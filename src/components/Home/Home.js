8b9eb9db24c112d669fc777e6f6ba526c5f0789e
nwre87 <nwre87@gmail.com>
2017-01-24 19:43:19 -1000

    add hr and styles

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,7 +18,8 @@ const HomeComponent = () => {
       <div className={styles.container}>
         <div className={styles.intro}>
           <h1 className={styles.heading}>Nigel Earle</h1>
-          <h2 className={styles.subHeading}>Sofware Developer</h2>
+          <h3 className={styles.subHeading}>Sofware Developer</h3>
+          <hr/>
           <p className={styles.description}>
             Software Developer specializing in JavaScript & Node.js. 
             As well as, other server side languages such as Python and Ruby. With some experience and strong interest in DevOps.
@@ -26,27 +27,27 @@ const HomeComponent = () => {
         </div>
         <ul className={styles.contact}>
           <li>
-            <a href="#" className={styles.socialLink}>
+            <a href="mailto:nigel@earle.io" className={styles.socialLink}>
               <img src={ENVELOPE_LOGO} alt="envelope" className={styles.logo}/>
             </a>
           </li>
           <li>
-            <a href="https://www.linkedin.com/in/nigelearle" className={styles.socialLink}>
+            <a target="_blank" href="https://www.linkedin.com/in/nigelearle" className={styles.socialLink}>
               <img src={LINKEDIN_LOGO} alt="github logo" className={styles.logo}/>
             </a>
           </li>
           <li>
-            <a href="https://github.com/NigelEarle" className={styles.socialLink}>
+            <a target="_blank" href="https://github.com/NigelEarle" className={styles.socialLink}>
               <img src={GITHUB_LOGO} alt="github logo" className={styles.logo}/>
             </a>
           </li>
           <li>
-            <a href="https://twitter.com/NigelEarle" className={styles.socialLink}>
+            <a target="_blank" href="https://twitter.com/NigelEarle" className={styles.socialLink}>
               <img src={TWITTER_LOGO} alt="twitter logo" className={styles.logo}/>
             </a>
           </li>
           <li>
-            <a href="http://stackoverflow.com/users/3996221/nigel-earle" className={styles.socialLink}>
+            <a target="_blank" href="http://stackoverflow.com/users/3996221/nigel-earle" className={styles.socialLink}>
               <img src={STACKOVERFLOW_LOGO} alt="github logo" className={styles.logo}/>
             </a>
           </li>
