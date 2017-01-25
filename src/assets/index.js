60e767c6b7dd2bfeebea19a4870cd0617f611717
nwre87 <nwre87@gmail.com>
2017-01-24 16:10:55 -1000

    add social icons

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import {default as DEFAULT_PERSON} from './images/default-person.png';
+import {default as ENVELOPE_LOGO} from './icons/envelope.svg';
+import {default as GITHUB_LOGO} from './icons/github-logo.svg';
+import {default as LINKEDIN_LOGO} from './icons/linkedin-logo.svg';
+import {default as TWITTER_LOGO} from './icons/twitter-logo.svg';
+
+export {
+  DEFAULT_PERSON,
+  ENVELOPE_LOGO,
+  GITHUB_LOGO,
+  LINKEDIN_LOGO,
+  TWITTER_LOGO,
+};
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import styles from './Home.css';
 import { HeaderComponent } from './components/../..';
-import DEFAULT_PERSON from './../../assets/images/default-person.png';
+
+import {
+  DEFAULT_PERSON,
+  ENVELOPE_LOGO,
+  GITHUB_LOGO,
+  LINKEDIN_LOGO,
+  TWITTER_LOGO,
+} from './../../assets';
 
 const HomeComponent = () => {
   return (
@@ -12,8 +19,16 @@ const HomeComponent = () => {
           <h1 className={styles.heading}>Nigel Earle</h1>
           <h2 className={styles.subHeading}>Sofware Developer</h2>
           <p className={styles.description}>
-            Software Developer specializing in JavaScript & Node.js. As well as, other server side languages such as Python and Ruby. With some experience and strong intrest in DevOps.
+            Software Developer specializing in JavaScript & Node.js. 
+            As well as, other server side languages such as Python and Ruby. With some experience and strong interest in DevOps.
           </p>
+        </div>
+        <div className={styles.contact}>
+          <a href="https://twitter.com/NigelEarle">
+          <img src="" alt=""/>
+          Twitter
+          </a> /
+          <a href="https://github.com/NigelEarle"></a>
         </div>
       </div>
     </div>
