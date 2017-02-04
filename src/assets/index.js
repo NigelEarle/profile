77ec836c883689a348267b22d191bf928acd0fea
nwre87 <nwre87@gmail.com>
2017-02-03 14:55:06 -1000

    styles for 404 not found page, patrick

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,7 @@ import {default as LINKEDIN_LOGO} from './icons/linkedin-logo.svg';
 import {default as TWITTER_LOGO} from './icons/twitter-logo.svg';
 import {default as STACKOVERFLOW_LOGO} from './icons/stackoverflow-logo.svg';
 import {default as NE_LOGO} from './icons/NE-logo.svg';
+import {default as PATRICK_KEYBOARD} from './icons/patrick-keyboard.svg';
 
 export {
   DEFAULT_PERSON,
@@ -16,4 +17,5 @@ export {
   STACKOVERFLOW_LOGO,
   HK_PANO,
   NE_LOGO,
+  PATRICK_KEYBOARD,
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import styles from './NotFound.css';
 
+import {PATRICK_KEYBOARD} from './../../assets';
+
 const NotFoundComponent = () => {
   return (
-    <div>
-      <h1>Not Found</h1>
+    <div className={styles.container}>
+      <h1 className={styles.title}>404 Not Found</h1>
+      <h3 className={styles.message}>Sorry! The page your looking for doesnt exist!</h3>
+      <img className={styles.patrick} src={PATRICK_KEYBOARD} alt="patrick keyboard"/>
     </div>
   );
 }
