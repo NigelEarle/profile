13316485554dc80324c5afc4074f5021ba0fe698
nwre87 <nwre87@gmail.com>
2017-02-04 15:03:50 -1000

    add styles to api gui

--- server.js
+++ server.js
@@ -40,7 +40,6 @@ app.use(passport.session());
 app.use(methodOverride(function (req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
     var method = req.body._method
-    console.log('middleware', method)
     delete req.body._method
     return method
   }
